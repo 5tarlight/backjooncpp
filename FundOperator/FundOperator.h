afb71f979a1c8feb34264932b447aa3a9068ec51
5tarlight <developerstarlight@gmail.com>
2021-02-04 17:14:20 +0900

    q10718

---
+++ FundOperator/FundOperator.h
@@ -0,0 +1,18 @@
+//
+// Created by devel on 2021-02-04.
+//
+
+#ifndef BACKJOON_FUNDOPERATOR_H
+#define BACKJOON_FUNDOPERATOR_H
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class FundOperator {
+public:
+  void q10718();
+};
+
+#endif //BACKJOON_FUNDOPERATOR_H
---
+++ FundOperator/q10718.cpp
@@ -0,0 +1,12 @@
+//
+// Created by devel on 2021-02-04.
+//
+
+#include "FundOperator.h"
+
+// https://www.acmicpc.net/problem/10718
+
+void FundOperator::q10718() {
+  cout << "강한친구 대한육군" << endl;
+  cout << "강한친구 대한육군" << endl;
+}
